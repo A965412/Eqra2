59943d2e471f73b3a2e9300d7c698cf62f46d1a4
ahmed <https://github.com/A965412/E-commerce_Application.git>
2024-10-06 01:00:10 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:eqra2/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const  MaterialApp(
+      debugShowCheckedModeBanner:  false,
+    
+      home:   Home_Screen(),
+    );
+  }
+}
+
